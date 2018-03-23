fcd10b024a15a7d8fdecb123610b03715400a690
Unknown <liyu4752@gmail.com>
2018-03-24 01:47:02 +0900

    Solve problem.

---
+++ Algorithm/Dog/main.cpp
@@ -0,0 +1,11 @@
+// 2018-03-24
+// https://www.acmicpc.net/problem/10172
+
+#include <stdio.h>
+
+int main()
+{
+	printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+
+	return 0;
+}
