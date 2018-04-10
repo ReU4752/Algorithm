49a1b09bdffdfe1b1399279f2e22ddde563eeec0
Unknown <liyu4752@gmail.com>
2018-04-10 14:20:39 +0900

    Optimize algorithm.

--- Algorithm/Tomato/main.cpp
+++ Algorithm/Tomato/main.cpp
@@ -24,6 +24,8 @@ queue<Space> g_spaceQueue;
 
 int GetLastRipenDay()
 {
+  const static int dirRow[4] = { -1, 1, 0, 0 };
+  const static int dirCol[4] = { 0, 0, -1, 1 };
   int lastDay = 0;
   int ripenCount = 0;
 
@@ -34,40 +36,25 @@ int GetLastRipenDay()
 
     g_spaceQueue.pop();
 
-    if (nowSpace.row > 0 && g_storage[nowSpace.row - 1][nowSpace.col] == 0)
+    for (int i = 0; i < 4; i++)
     {
-      g_spaceQueue.push(Space(nowSpace.day + 1, nowSpace.row - 1, nowSpace.col));
-      g_storage[nowSpace.row - 1][nowSpace.col] = 1;
+      if (nowSpace.row + dirRow[i] >= 0 
+          && nowSpace.row + dirRow[i] <= height - 1
+          && nowSpace.col + dirCol[i] >= 0
+          && nowSpace.col + dirCol[i] <= width - 1
+          && g_storage[nowSpace.row + dirRow[i]][nowSpace.col + dirCol[i]] == 0)
+      {
+        g_spaceQueue.push(Space(nowSpace.day + 1, nowSpace.row + dirRow[i], nowSpace.col + dirCol[i]));
+        g_storage[nowSpace.row + dirRow[i]][nowSpace.col + dirCol[i]] = 1;
 
-      ripenCount++;
-    }
-    if (nowSpace.row < height - 1 && g_storage[nowSpace.row + 1][nowSpace.col] == 0)
-    {
-      g_spaceQueue.push(Space(nowSpace.day + 1, nowSpace.row + 1, nowSpace.col));
-      g_storage[nowSpace.row + 1][nowSpace.col] = 1;
-
-      ripenCount++;
-    }
-    if (nowSpace.col > 0 && g_storage[nowSpace.row][nowSpace.col - 1] == 0)
-    {
-      g_spaceQueue.push(Space(nowSpace.day + 1, nowSpace.row, nowSpace.col - 1));
-      g_storage[nowSpace.row][nowSpace.col - 1] = 1;
-
-      ripenCount++;
-    }
-    if (nowSpace.col < width - 1 && g_storage[nowSpace.row][nowSpace.col + 1] == 0)
-    {
-      g_spaceQueue.push(Space(nowSpace.day + 1, nowSpace.row, nowSpace.col + 1));
-      g_storage[nowSpace.row][nowSpace.col + 1] = 1;
-
-      ripenCount++;
+        ripenCount++;
+      }
     }
   }
 
+  // 처음에 익지 못한 개수와 마지막에 익어진 개수가 다르면 전부 익지 못한것.
   if (lessRipenCount != ripenCount)
-  {
     lastDay = 0;
-  }
 
   return lastDay;
 }
@@ -76,12 +63,14 @@ int main()
 {
   scanf("%d %d", &width, &height);
 
-  g_storage.reserve(width * height);
+  g_storage.reserve(height);
 
   for (int i = 0; i < height; i++)
   {
     vector<int> temp;
     int state;
+
+    temp.reserve(width);
 
     for (int j = 0; j < width; j++)
     {
@@ -92,11 +81,9 @@ int main()
       case -1:
         blankCount++;
         break;
-
       case 0:
         lessRipenCount++;
         break;
-
       case 1:
         g_spaceQueue.push(Space(0, i, j));
         break;
@@ -108,7 +95,8 @@ int main()
     g_storage.push_back(temp);
   }
 
-  if (width * height - blankCount == g_spaceQueue.size()) // 처음부터 모두 익어있는 상황.
+  // 처음부터 모두 익어있는 상황이면.
+  if (width * height - blankCount == g_spaceQueue.size())
     printf("0\n");
   else
   {
