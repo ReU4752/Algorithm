1be5ce0d8667a98f1728e62a8a538e53691c4e29
Unknown <liyu4752@gmail.com>
2018-04-20 10:49:22 +0900

    Solve problem.

---
+++ Algorithm/PrintStar_2/main.cpp
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/2439
+// 2018-04-20
+
+#include <stdio.h>
+
+int main()
+{
+  int n;
+
+  scanf("%d", &n);
+
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      if (j < n - i)
+        printf(" ");
+      else
+        printf("*");
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
